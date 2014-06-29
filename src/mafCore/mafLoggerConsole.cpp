ae726ce38473e0eddb09c32a280d9e76c2cd16cf
Daniele Giunchi <dannox@gmail.com>
2014-06-30 09:51:17 +1000

    fixed console crash on vs2013

--- src/mafCore/mafLoggerConsole.cpp
+++ src/mafCore/mafLoggerConsole.cpp
@@ -22,27 +22,32 @@ mafLoggerConsole::~mafLoggerConsole() {
 
 void mafLoggerConsole::loggedMessage(const QtMsgType type, const QString &msg) {
 	QString dt(QDateTime::currentDateTime().toString(mafDateTimeLogFormat));
-	std::cout << dt.toStdString() << " " << "---> ";
+	std::string cp; 
+	cp.append(QDateTime::currentDateTime().toString(mafDateTimeLogFormat).toLatin1());
+	cp.append(" ---> ");
+	std::cout << cp << " " << "---> ";
+	std::string message;
+	message.append(msg.toLatin1());
     switch (type) {
     case QtDebugMsg:
         if(logMode() == mafLogModeTestSuite) {
-			std::cout << TEST_SUITE_LOG_PREFIX << " " << msg.toStdString() << std::endl;
+			std::cout << TEST_SUITE_LOG_PREFIX << " " << message << std::endl;
         } else {
-		    std::cout << "Debug:" << msg.toStdString() << std::endl;
+			std::cout << "Debug:" << message << std::endl;
         }
         break;
     case QtWarningMsg:
         if(logMode() == mafLogModeTestSuite) {
-			std::cout << TEST_SUITE_LOG_PREFIX << " " << msg.toStdString() << std::endl;
+			std::cout << TEST_SUITE_LOG_PREFIX << " " << message << std::endl;
         } else {
-			std::cout << "Warning:" << msg.toStdString() << std::endl;
+			std::cout << "Warning:" << message << std::endl;
         }
         break;
     case QtCriticalMsg:
-		std::cout << "Critical:" << msg.toStdString() << std::endl;
+		std::cout << "Critical:" << message << std::endl;
         break;
     case QtFatalMsg:
-		std::cout << "Fatal:" << msg.toStdString() << std::endl;
+		std::cout << "Fatal:" << message << std::endl;
         //abort();
         break;
     }
